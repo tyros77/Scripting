0f69564ffbf50d4497e8fe57d1984304265f5271
tyros77 <78060037+tyros77@users.noreply.github.com>
2021-02-02 00:15:13 -0700

    Create kerb_search.ps1

---
+++ Powershell/kerb_search.ps1
@@ -0,0 +1,13 @@
+# Author: tyros77
+# Script to parse DC security log for TGS requests to identify Kerberos attacks
+
+$cpname = $args[0]
+$t = $args[1]
+$start = (Get-Date).AddHours(-$t)
+$properties = @(
+	@{n='EventID';e={$_.ID}}
+	@{n='Date';e={$_.TimeCreated}}
+	@{n='AccountName';e={$_.Properties[0].Value}}
+	@{n='ServiceName';e={$_.Properties[2].Value}}
+	@{n='Encryption';e={$_.Properties[5].Value | Format-Hex | Select-Object -Expand Bytes | ForEach-Object { '0x{0:x2}' -f $_ } -join ''}})
+Get-WinEvent -ComputerName $cpname -FilterHashTable @{LogName='Security'; ID=4769; StartTime=$start} | Select-Object $properties | Format-Table -AutoSize
